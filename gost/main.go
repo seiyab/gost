3d49f01d96af7d0f0b83865b372d6ffdc694f280
seiya <20365512+seiyab@users.noreply.github.com>
2023-09-18 15:37:53 +0900

    add analyzers into main

--- gost/main.go
+++ gost/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"github.com/seiyab/gost/gost/multipleerrors"
 	"github.com/seiyab/gost/gost/nodiscarderror"
 	"github.com/seiyab/gost/gost/openfileflag"
+	"github.com/seiyab/gost/gost/wraperror"
 	"golang.org/x/tools/go/analysis/multichecker"
 )
 
@@ -10,5 +12,7 @@ func main() {
 	multichecker.Main(
 		openfileflag.Analyzer,
 		nodiscarderror.Analyzer,
+		multipleerrors.Analyzer,
+		wraperror.Analyzer,
 	)
 }
