03ebfa049a131b129d1b5e8753ae48923eff7b1a
seiya <20365512+seiyab@users.noreply.github.com>
2024-03-03 22:48:37 +0900

    add failing test

--- gost/openfileflag/testdata/a.go
+++ gost/openfileflag/testdata/a.go
@@ -13,4 +13,13 @@ func _() {
 
 	os.OpenFile("", os.O_WRONLY, 0600)             // want ".+"
 	os.OpenFile("", os.O_WRONLY|os.O_CREATE, 0600) // want ".+"
+
+	f1, err1 := os.OpenFile("", os.O_WRONLY, 0600) // want ".+"
+	f1, err1 = os.OpenFile("", os.O_WRONLY, 0600)  // want ".+"
+	_, err2 := os.OpenFile("", os.O_WRONLY, 0600)
+	_, err2 = os.OpenFile("", os.O_WRONLY, 0600)
+
+	markAsUsed(f1, err1, err2)
 }
+
+func markAsUsed(_ ...any) {}
